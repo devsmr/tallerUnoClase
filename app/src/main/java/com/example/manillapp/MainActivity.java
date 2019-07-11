bf7d0f43011ce09e48b11591627abc48b07dab8e
leandro gutierrez <leandro9115@gmail.com>
2019-07-10 23:56:39 -0500

    desarrollo inicial

--- app/src/main/java/com/example/manillapp/MainActivity.java
+++ app/src/main/java/com/example/manillapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.manillapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -9,11 +10,13 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
     TextView resultado;
     EditText cantidadIngresada;
-
     String opcMateriales[],opcDije[],opcTipoDije[],opcTipoMoneda[];
     Spinner spMateriales,spDije,spTipoDije,spTipoMoneda;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,17 +24,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Captura de los objetos utilizados del spinner
-        cantidadIngresada = findViewById(R.id.cantidad);
+
         spMateriales = findViewById((R.id.material));
         spDije = findViewById((R.id.dije));
         spTipoDije = findViewById(R.id.tipoDije);
         spTipoMoneda = findViewById(R.id.tipoMoneda);
+        cantidadIngresada = findViewById(R.id.cantidadIng);
+        resultado = findViewById(R.id.resultadoConsult);
 
 
         //Traemos las opciones de un array Strings (valores del spinner)
         opcMateriales = getResources().getStringArray(R.array.material);
         opcDije = getResources().getStringArray(R.array.dije);
-        opcTipoDije = getResources().getStringArray(R.array.tipoManilla);
+        opcTipoDije = getResources().getStringArray(R.array.tipoDije);
         opcTipoMoneda = getResources().getStringArray(R.array.tipoMoneda);
 
 
@@ -48,5 +53,130 @@ public class MainActivity extends AppCompatActivity {
         spTipoMoneda.setAdapter(adapter4);
 
 
+    }
+
+
+    public void consultar (View v){
+        int resultConsulta = 0;
+        int dolar = 3200;
+        int opcMoneda,caseMateriales,caseDije,caseTipoDije,caseCantidad;
+
+
+       //Guardar las posiciones que tienen los spinners
+        opcMoneda = spTipoMoneda.getSelectedItemPosition();
+        caseMateriales =spMateriales.getSelectedItemPosition();
+        caseDije = spDije.getSelectedItemPosition();
+        caseTipoDije = spTipoDije.getSelectedItemPosition();
+        caseCantidad = Integer.parseInt(cantidadIngresada.getText().toString());
+
+
+        switch (opcMoneda){
+
+            // 0 = pesos colombiandos
+            // 1 = dolares
+            case 0:
+                if(((caseMateriales == 0)&& (caseDije == 0)) &&
+                        (caseTipoDije == 0 || caseTipoDije == 1)){
+                            resultConsulta = caseCantidad * 100;
+                            break;
+
+                }else if(((caseMateriales == 0)&& (caseDije == 0)) && (caseTipoDije == 2)){
+                            resultConsulta = caseCantidad * 80;
+                            break;
+                }else if(((caseMateriales == 0)&& (caseDije == 0)) && (caseTipoDije == 3)){
+                       resultConsulta = caseCantidad * 70;
+                       break;
+                }else if(((caseMateriales == 0)&& (caseDije == 1)) &&
+                        (caseTipoDije == 0 || caseTipoDije == 1)){
+                         resultConsulta = caseCantidad * 120;
+                    break;
+                }else if(((caseMateriales == 0)&& (caseDije == 1)) && (caseTipoDije == 2)){
+                    resultConsulta = caseCantidad * 100;
+                    break;
+                }else if(((caseMateriales == 0)&& (caseDije == 1)) && (caseTipoDije == 3)){
+                    resultConsulta = caseCantidad * 90;
+                    break;
+                }//
+                 else if(((caseMateriales == 1)&& (caseDije == 0)) &&
+                        (caseTipoDije == 0 || caseTipoDije == 1)){
+                    resultConsulta = caseCantidad * 90;
+                    break;
+
+                }else if(((caseMateriales == 1)&& (caseDije == 0)) && (caseTipoDije == 2)){
+                    resultConsulta = caseCantidad * 70;
+                    break;
+                }else if(((caseMateriales == 1)&& (caseDije == 0)) && (caseTipoDije == 3)){
+                    resultConsulta = caseCantidad * 50;
+                    break;
+                }else if(((caseMateriales == 1)&& (caseDije == 1)) &&
+                        (caseTipoDije == 0 || caseTipoDije == 1)){
+                    resultConsulta = caseCantidad * 110;
+                    break;
+                }else if(((caseMateriales == 1)&& (caseDije == 1)) && (caseTipoDije == 2)){
+                    resultConsulta = caseCantidad * 90;
+                    break;
+                }else if(((caseMateriales == 1)&& (caseDije == 1)) && (caseTipoDije == 3)){
+                    resultConsulta = caseCantidad * 80;
+                    break;
+                }
+                //
+
+
+            case 1:
+                if(((caseMateriales == 0)&& (caseDije == 0)) &&
+                        (caseTipoDije == 0 || caseTipoDije == 1)){
+                    resultConsulta = (caseCantidad * 100) * dolar;
+                    break;
+
+                }else if(((caseMateriales == 0)&& (caseDije == 0)) && (caseTipoDije == 2)){
+                    resultConsulta = (caseCantidad * 80)  * dolar;
+                    break;
+                }else if(((caseMateriales == 0)&& (caseDije == 0)) && (caseTipoDije == 3)){
+                    resultConsulta = (caseCantidad * 70)  * dolar;
+                    break;
+                }else if(((caseMateriales == 0)&& (caseDije == 1)) &&
+                        (caseTipoDije == 0 || caseTipoDije == 1)){
+                    resultConsulta = (caseCantidad * 120 )  * dolar;
+                    break;
+                }else if(((caseMateriales == 0)&& (caseDije == 1)) && (caseTipoDije == 2)){
+                    resultConsulta = (caseCantidad * 100)  * dolar;
+                    break;
+                }else if(((caseMateriales == 0)&& (caseDije == 1)) && (caseTipoDije == 3)){
+                    resultConsulta = (caseCantidad * 90)  * dolar;
+                    break;
+                }//
+                else if(((caseMateriales == 1)&& (caseDije == 0)) &&
+                        (caseTipoDije == 0 || caseTipoDije == 1)){
+                    resultConsulta = (caseCantidad * 90)  * dolar;
+                    break;
+
+                }else if(((caseMateriales == 1)&& (caseDije == 0)) && (caseTipoDije == 2)){
+                    resultConsulta = (caseCantidad * 70)  * dolar;
+                    break;
+                }else if(((caseMateriales == 1)&& (caseDije == 0)) && (caseTipoDije == 3)){
+                    resultConsulta = (caseCantidad * 50)  * dolar;
+                    break;
+                }else if(((caseMateriales == 1)&& (caseDije == 1)) &&
+                        (caseTipoDije == 0 || caseTipoDije == 1)){
+                    resultConsulta = (caseCantidad * 110)  * dolar;
+                    break;
+                }else if(((caseMateriales == 1)&& (caseDije == 1)) && (caseTipoDije == 2)){
+                    resultConsulta = (caseCantidad * 90)  * dolar;
+                    break;
+                }else if(((caseMateriales == 1)&& (caseDije == 1)) && (caseTipoDije == 3)){
+                    resultConsulta = (caseCantidad * 80)  * dolar;
+                    break;
+                }
+                //
+
+
+
+        }
+        resultado.setText("" + resultConsulta);
+
+    }
+
+    public void cancelar (View v){
+        spMateriales.setSelection(0);
     }
 }
